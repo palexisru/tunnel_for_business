1a5115d54208a61f81be73244bc5c12e91c7377d
palexisru <a_podorov@mail.ru>
2024-07-09 14:49:28 +0300

    Phase added

---
+++ v01/ai_test/ai_test/Base/Phase/Phase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ai_test.Base
+{
+    public enum Phase
+    {
+        None = 0,
+        Past = 3,
+        Current = 2,
+        Future = 1,
+        Infinitive = 0,
+        Mask = 3
+    }
+}
